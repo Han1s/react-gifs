5ea474bf7b64d63be987ef201c04bb51f26dfa74
Jan Ranostaj <HonzaRanostaj@gmail.com>
2019-06-08 20:08:40 +0200

    create app component

---
+++ src/components/app.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <div className="left-scene" />
+//         <div className="right-scene" />
+//       </div>
+//     )
+//   }
+// }
+
+const App = () => {
+  return (
+    <div>
+      <div className="left-scene" />
+      <div className="right-scene" />
+    </div>
+  );
+};
+
+export default App;
